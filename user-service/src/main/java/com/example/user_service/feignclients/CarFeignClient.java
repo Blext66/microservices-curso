e0f3ab4a243c5209e645ccd6bb0507f8598151c7
Blext66 <154301747+Blext66@users.noreply.github.com>
2024-08-13 18:40:23 -0500

    Inicio del Proyecto

---
+++ user-service/src/main/java/com/example/user_service/feignclients/CarFeignClient.java
@@ -0,0 +1,18 @@
+package com.example.user_service.feignclients;
+
+import com.example.user_service.model.Car;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "car-service", url = "http://localhost:8002")
+@RequestMapping("/car")
+public interface CarFeignClient {
+
+    @PostMapping()
+    Car save(@RequestBody Car car);
+
+    @GetMapping("/byuser/{userId}")
+    List<Car> getCars(@PathVariable("userId") int userId);
+}
